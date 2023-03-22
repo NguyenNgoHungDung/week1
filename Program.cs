37f6a0353cd1f5c9d0861ebc5fbbdcf7f0a5ab6c
dungnnh <dung.nnh5930@sinhvien.hoasen.edu.vn>
2023-03-22 08:27:24 +0700

    cap nhat

--- Program.cs
+++ Program.cs
@@ -10,9 +10,37 @@
 //         }
 //     }
 // }
-Console.WriteLine("What is your name?");
-var name = Console.ReadLine();
-var currentDate = DateTime.Now;
-Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-Console.Write($"{Environment.NewLine}Press any key to exit...");
-Console.ReadKey(true);
+// Console.WriteLine("What is your name?");
+// var name = Console.ReadLine();
+// var currentDate = DateTime.Now;
+// Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+// Console.Write($"{Environment.NewLine}Press any key to exit...");
+// Console.ReadKey(true);
+// string firstName = "Maria";
+// string lastName = "Sage";
+// string fullName = firstName + lastName;
+// Console.WriteLine($"The first name has {firstName.Length} letters.");
+
+// string greeting = "      Hello World!       ";
+// string trimmedGreeting = greeting.TrimStart();
+// trimmedGreeting = greeting.TrimEnd();
+// trimmedGreeting = greeting.Trim();
+
+// string Hello = "Hello World!";
+// Hello = Hello.Replace("Hello", "Greetings");
+
+// Console.WriteLine(Hello.ToUpper());
+// Console.WriteLine(Hello.ToLower());
+
+int a = 7;
+int b = 4;
+int c = 3;
+int d = (a + b) / c;
+int e = (a + b) % c;
+Console.WriteLine($"quotient: {d}");
+Console.WriteLine($"remainder: {e}");
+int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
